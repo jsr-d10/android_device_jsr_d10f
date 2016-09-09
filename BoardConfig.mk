cd65c133500fd443436c446ac6fb84081e1bcd3b
remittor <remittor@gmail.com>
2016-09-09 14:48:04 +0300

    [BOARDCONFIG] Switch to use UBERTC 4.9.x

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,7 @@ TARGET_LIBINIT_DEFINES_FILE := device/jsr/d10f/init/init_d10f.c
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/jsr/msm8228
 TARGET_KERNEL_CONFIG := jsr_d10f_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_KERNEL_ARCH := arm
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=d10f androidboot.selinux=permissive
