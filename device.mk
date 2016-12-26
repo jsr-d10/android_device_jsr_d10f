ce7c6c42f0007acb31af80c871bc0bc4322b91b6
S-trace <S-trace@list.ru>
2016-12-26 20:19:14 +0300

    [OTA] Include OpenDelta package

--- device.mk
+++ device.mk
@@ -144,6 +144,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=true \
     media.stagefright.less-secure=true
 
+# OpenDelta
+PRODUCT_PACKAGES += \
+    OpenDelta
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
