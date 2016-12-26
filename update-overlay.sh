7f87b4390fb4bab3bb88573c91be23ac9417a0eb
S-trace <S-trace@list.ru>
2016-12-26 21:46:07 +0300

    [OVERLAY] Settings: Show JSR_Settings page in "About phone"
    Overriding additional_system_update_settings is much easier than adding new entry in main menu

--- update-overlay.sh
+++ update-overlay.sh
@@ -11,4 +11,5 @@ xmlstarlet ed --pf --ps \
 -u '/PreferenceScreen/cyanogenmod.preference.SelfRemovingPreference[@android:key="cm_updates"]/@android:title'                -v "@string/system_update_settings_list_item_title" \
 -u '/PreferenceScreen/cyanogenmod.preference.SelfRemovingPreference[@android:key="cm_updates"]/intent/@android:targetPackage' -v "eu.chainfire.opendelta" \
 -u '/PreferenceScreen/cyanogenmod.preference.SelfRemovingPreference[@android:key="cm_updates"]/intent/@android:targetClass'   -v "eu.chainfire.opendelta.MainActivity" \
+-u '/PreferenceScreen/PreferenceScreen[@android:key="additional_system_update_settings"]/@android:title'                      -v "@string/wifi_show_advanced" \
 ../../../packages/apps/Settings/res/xml/device_info_settings.xml > overlay/packages/apps/Settings/res/xml/device_info_settings.xml
