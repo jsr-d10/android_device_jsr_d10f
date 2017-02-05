LineageOS/android_device_xiaomi_armani

armani: Rebrand to LineageOS
Change-Id: I291f2d93d17c2566ed358982dc310fb3679cb9d1

lineage.mk
@@ -1,5 +1,5 @@
-#
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/armani/device.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
 
-# Inherit some common CM stuff
+# Inherit some common LineageOS stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := armani
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := lineage_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM 1S
 PRODUCT_MANUFACTURER := Xiaomi

jsr-d10/android_device_jsr_d10f

lineage.mk
@@ -1,5 +1,5 @@
-#
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/jsr/d10f/device.mk)
 $(call inherit-product-if-exists, vendor/jsr/d10f/d10f-vendor.mk)
 
-# Inherit some common CM stuff
+# Inherit some common LineageOS stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := d10f
-PRODUCT_NAME := cm_d10f
+PRODUCT_NAME := lineage_d10f
 PRODUCT_BRAND := JSR
 PRODUCT_MODEL := D10F
 PRODUCT_MANUFACTURER := JSR Tech