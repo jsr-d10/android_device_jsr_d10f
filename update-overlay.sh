3fd8c1a05c5bd76def48153d62fe644dd33ad990
S-trace <S-trace@list.ru>
2017-02-15 12:00:58 +0300

    [OVERLAY] packages/apps/Settings: Do not change updater activity
    This leads to doubling updater items in Settings

--- update-overlay.sh
+++ update-overlay.sh
@@ -8,9 +8,5 @@ cd "$PATCHBASE"
 mkdir -p overlay/packages/apps/Settings/res/xml/
 
 xmlstarlet ed --pf --ps \
--u '/PreferenceScreen/cyanogenmod.preference.SelfRemovingPreference[@android:key="cm_updates"]/@android:title'                -v "@string/system_update_settings_list_item_title" \
--u '/PreferenceScreen/cyanogenmod.preference.SelfRemovingPreference[@android:key="cm_updates"]/@cm:requiresPackage'           -v "eu.chainfire.opendelta" \
--u '/PreferenceScreen/cyanogenmod.preference.SelfRemovingPreference[@android:key="cm_updates"]/intent/@android:targetPackage' -v "eu.chainfire.opendelta" \
--u '/PreferenceScreen/cyanogenmod.preference.SelfRemovingPreference[@android:key="cm_updates"]/intent/@android:targetClass'   -v "eu.chainfire.opendelta.MainActivity" \
--u '/PreferenceScreen/PreferenceScreen[@android:key="additional_system_update_settings"]/@android:title'                      -v "@string/wifi_show_advanced" \
+-u '/PreferenceScreen/PreferenceScreen[@android:key="additional_system_update_settings"]/@android:title' -v "@string/wifi_show_advanced" \
 ../../../packages/apps/Settings/res/xml/device_info_settings.xml > overlay/packages/apps/Settings/res/xml/device_info_settings.xml
