9b6647168ac09146fb0ea0b379206ccfe61667a6
S-trace <S-trace@list.ru>
2017-02-15 10:56:52 +0300

    [OVERLAY] packages/apps/CMFileManager: Fix CMFileManager
    // и да, я не смог в xmlstarlet >_<

--- update-overlay.sh
+++ update-overlay.sh
@@ -10,3 +10,6 @@ mkdir -p overlay/packages/apps/Settings/res/xml/
 xmlstarlet ed --pf --ps \
 -u '/PreferenceScreen/PreferenceScreen[@android:key="additional_system_update_settings"]/@android:title' -v "@string/wifi_show_advanced" \
 ../../../packages/apps/Settings/res/xml/device_info_settings.xml > overlay/packages/apps/Settings/res/xml/device_info_settings.xml
+
+mkdir -p overlay/packages/apps/CMFileManager/res/xml/
+sed 's!%1\$s 1&gt; /dev/null \&amp;\&amp; /system/bin/stat -t %1!%1$s 2>\&amp;1 1\&gt; /dev/null | grep -Fv /.android_secure || /system/bin/stat -t %1!'  < ../../../packages/apps/CMFileManager/res/xml/command_list.xml > overlay/packages/apps/CMFileManager/res/xml/command_list.xml
