451798706dfd2f4ac03df0dc60b167b0da4b0072
S-trace <S-trace@list.ru>
2017-02-25 02:01:34 +0300

    [FSTAB] Convert fstab generator from init patch to standalone binary
    Backported from LineageOS 14.1 branch.
    Origin:
    https://github.com/jsr-d10/android_device_jsr_d10f/tree/994349cadaaec8e7305651405b95ed4f3d80a431/fstab

---
+++ fstab/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE           := fstab
+LOCAL_MODULE_TAGS      := optional
+LOCAL_SRC_FILES        := fstab.c log.c
+LOCAL_CFLAGS           += -Wall -Wextra
+LOCAL_STATIC_LIBRARIES := libcutils libbase
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+include $(BUILD_EXECUTABLE)
