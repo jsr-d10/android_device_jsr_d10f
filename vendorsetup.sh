69522179bef4e4013b3af7d0543ecabcc6103e63
S-trace <S-trace@list.ru>
2017-11-05 09:46:29 +0300

    [VENDORSETUP] Fix typo in prebuilts/qemu-kernel/arm/ creation
    mkdir have no -f option
    
    Change-Id: I8f18557d23df2ff5bae4c078abaf60ee49aa3b26

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,7 +20,7 @@ rm -rf out/target/product/d10f/obj/PACKAGING/target_files_intermediates
 rm -f out/target/product/d10f/system/build.prop
 rm -f out/target/product/d10f/root/default.prop
 
-[ -d prebuilts/qemu-kernel/arm/ ] || mkdir -fp prebuilts/qemu-kernel/arm/
+[ -d prebuilts/qemu-kernel/arm/ ] || mkdir -p prebuilts/qemu-kernel/arm/
 [ -f prebuilts/qemu-kernel/arm/LINUX_KERNEL_COPYING ] || touch prebuilts/qemu-kernel/arm/LINUX_KERNEL_COPYING
 
 [ -d vendor/cm/proprietary/ ] || mkdir vendor/cm/proprietary/
