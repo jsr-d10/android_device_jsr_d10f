9af4cccf0999fa65c6c2fb44c2193e39b077b274
S-trace <S-trace@list.ru>
2018-01-03 18:03:00 +0300

    [SETTINGS] SettingsActivity: Fix possible NPE
    Change-Id: Ic48f6147742d1335134080df23645997c8e82352

--- JSR_Settings/java/com/cyanogenmod/settings/device/SettingsActivity.java
+++ JSR_Settings/java/com/cyanogenmod/settings/device/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.cyanogenmod.settings.device;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.view.MenuItem;
@@ -11,7 +12,9 @@ public class SettingsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment()).commit();
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
