460e2f452a60c95066fbb9a87c069e6b17132423
S-trace <S-trace@list.ru>
2018-02-26 21:18:49 +0300

    [VENDORSETUP] Change cm_d10f-* to lineage_d10f-*

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -60,6 +60,6 @@ sh device/jsr/d10f/update-overlay.sh
 
 rm -f out/target/product/d10f/system/build.prop out/target/product/d10f/boot/root/default.prop out/target/product/d10f/recovery/root/default.prop
 
-add_lunch_combo cm_d10f-user
-add_lunch_combo cm_d10f-userdebug
-add_lunch_combo cm_d10f-eng
+add_lunch_combo lineage_d10f-user
+add_lunch_combo lineage_d10f-userdebug
+add_lunch_combo lineage_d10f-eng
